95d37ec0cd51c70ca25a5cfa9d7e571b470bdf0c
Ashwani1330 <ashwanihabri@gmail.com>
2025-05-31 23:56:43 +0530

    feat: UI Info-Canvas + activating buttons added on 3d models

--- Assets/Scripts/BackNavigation.cs
+++ Assets/Scripts/BackNavigation.cs
@@ -7,10 +7,25 @@ public class BackNavigation : MonoBehaviour
     public ARSession arSession;
     public string previousSceneName = "StartScene";
 
+    public GameObject infoCanvas;
+    public GameObject quizCanvas;
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
+            // Check if any UICanvasTag is active
+            var allUICanvases = FindObjectsOfType<UICanvasTag>();
+            foreach (var canvas in allUICanvases)
+            {
+                if (canvas.gameObject.activeSelf)
+                {
+                    canvas.gameObject.SetActive(false);
+                    return;
+                }
+            }
+            
+            // No UI canvas open -> exit AR session
             Debug.Log("Back button detected in AR Session. Navigating to " + previousSceneName);
             if (arSession != null)
             {
