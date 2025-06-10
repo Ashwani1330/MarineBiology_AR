59e367695a3bbce4fe5106441d20616d83165ade
Ashwani1330 <ashwanihabri@gmail.com>
2025-06-10 16:00:31 +0530

    feat: Shark follows octopus & reacts to ink

--- Assets/OctopusInk.cs
+++ Assets/OctopusInk.cs
@@ -3,10 +3,12 @@ using UnityEngine;
 public class OctopusInk : MonoBehaviour
 {
     public GameObject inkParticlePrefab;
+    public SharkFollower shark;
 
     public void SpewInk()
     {
         Instantiate(inkParticlePrefab, transform.position, Quaternion.identity);
         // Add shark reaction here if needed
+        shark.OnInkHit();
     }
 }
